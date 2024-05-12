1ccfddad9751cf50ed0bd319a981c8b62b2e9408
Matthew Kandiah <mj.kandiah@googlemail.com>
2024-05-12 17:52:26 +0100

    with the keyboard stagger, this is more comfortable

--- keyboards/framework/iso/keymaps/MatthewKandiah/keymap.c
+++ keyboards/framework/iso/keymaps/MatthewKandiah/keymap.c
@@ -61,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_COMM,    KC_7,    KC_8,    KC_9,    KC_P,    KC_LBRC, KC_RBRC,
         KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_0,    KC_4,    KC_5,    KC_6,    KC_MINS, KC_QUOT, KC_NUHS, KC_ENT,
-        KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_DOT,    KC_1,    KC_2,    KC_3, KC_DOT,  KC_SLSH,     KC_RSFT,
+        KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_DOT,    KC_1,    KC_2,    KC_3, KC_SLSH,     KC_RSFT,
         KC_LCTL, MO(_FN), KC_LGUI, KC_LALT,          KC_SPC,                    KC_RALT, KC_RCTL, KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT
     ),
 
