09306c343e957968a14f999c354dc4cf059a9bb2
Matthew Kandiah <mj.kandiah@googlemail.com>
2024-05-19 10:40:58 +0100

    make layers more similar to colemak layer

--- keyboards/framework/iso/keymaps/MatthewKandiah/keymap.c
+++ keyboards/framework/iso/keymaps/MatthewKandiah/keymap.c
@@ -35,29 +35,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_SYMBOLS] = LAYOUT(
         KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
-        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_CAPS,
         KC_TAB,  KC_EXLM,    KC_DQUO,    KC_LCBR,    KC_RCBR,    KC_PIPE,    KC_NUBS,    KC_LT,    KC_GT,    KC_EQL,    KC_PLUS,    KC_LBRC, KC_RBRC,
-        KC_CAPS, KC_PERC,    LSFT(KC_NUBS),    KC_LPRN,    KC_RPRN,    KC_GRV,    KC_SLSH,    KC_QUOT,    KC_AT,    KC_MINS,    KC_UNDS, KC_QUOT, KC_NUHS, KC_ENT,
+        KC_ESC, KC_PERC,    LSFT(KC_NUBS),    KC_LPRN,    KC_RPRN,    KC_GRV,    KC_SLSH,    KC_QUOT,    KC_AT,    KC_MINS,    KC_UNDS, KC_QUOT, KC_NUHS, KC_ENT,
         KC_LSFT, KC_CIRC, KC_DLR,    KC_LBRC,    KC_RBRC,    LSFT(KC_3),    KC_NO,    KC_GRV,    KC_AMPR,    KC_NUHS, KC_ASTR,  KC_QUES,     KC_RSFT,
-        KC_LCTL, MO(_FN), KC_LGUI, KC_LALT,          KC_SPC,                    KC_RALT, KC_RCTL, KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT
+        KC_LCTL, MO(_FN), KC_LGUI, KC_BSPC,          KC_SPC,                    KC_ENT, KC_RCTL, KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT
     ),
 
     [_NAVIGATION] = LAYOUT(
         KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
-        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_CAPS,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_PGUP,    KC_UP,    KC_PGDN,    KC_P,    KC_LBRC, KC_RBRC,
-        KC_CAPS, KC_LGUI,    KC_LALT,    KC_LSFT,    KC_LCTL,    KC_G,    KC_H,    KC_LEFT,    KC_DOWN,    KC_RIGHT,    KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,
+        KC_ESC, KC_LGUI,    KC_LALT,    KC_LSFT,    KC_LCTL,    KC_G,    KC_H,    KC_LEFT,    KC_DOWN,    KC_RIGHT,    KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,
         KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,     KC_RSFT,
-        KC_LCTL, MO(_FN), KC_LGUI, KC_LALT,          KC_SPC,                    KC_RALT, KC_RCTL, KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT
+        KC_LCTL, MO(_FN), KC_LGUI, KC_BSPC,          KC_SPC,                    KC_ENT, KC_RCTL, KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT
     ),
 
     [_NUMPAD] = LAYOUT(
         KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
-        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_CAPS,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_COMM,    KC_7,    KC_8,    KC_9,    KC_P,    KC_LBRC, KC_RBRC,
-        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_0,    KC_4,    KC_5,    KC_6,    KC_MINS, KC_QUOT, KC_NUHS, KC_ENT,
+        KC_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_0,    KC_4,    KC_5,    KC_6,    KC_MINS, KC_QUOT, KC_NUHS, KC_ENT,
         KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_DOT,    KC_1,    KC_2,    KC_3, KC_SLSH,     KC_RSFT,
-        KC_LCTL, MO(_FN), KC_LGUI, KC_LALT,          KC_SPC,                    KC_RALT, KC_RCTL, KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT
+        KC_LCTL, MO(_FN), KC_LGUI, KC_BSPC,          KC_SPC,                    KC_ENT, KC_RCTL, KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT
     ),
 
     [_FN] = LAYOUT(
