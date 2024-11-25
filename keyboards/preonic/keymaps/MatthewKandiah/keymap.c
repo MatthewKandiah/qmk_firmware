9c57e15ed4720236f7b342b8ab0731eae0c4ebf4
Matthew Kandiah <mj.kandiah@googlemail.com>
2024-11-25 21:48:58 +0000

    same tweak on preonic keyboard keymap

--- keyboards/preonic/keymaps/MatthewKandiah/keymap.c
+++ keyboards/preonic/keymaps/MatthewKandiah/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_BASE_MAC] = LAYOUT_ortho_5x12(
         KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F11, KC_F12, KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10,
         KC_Q,   KC_W,   KC_F,   KC_P,   KC_B,   KC_NO,  KC_NO,  KC_J,   KC_L,   KC_U,   KC_Y,   KC_SCLN,
-        CTLA,   ALTR,   SFTS,   GUIT,   KC_G,   KC_NO,  KC_NO,  KC_M,   GUIN,   SFTE,   ALTI,   CTLO,
+        GUIA,   ALTR,   SFTS,   CTLT,   KC_G,   KC_NO,  KC_NO,  KC_M,   CTLN,   SFTE,   ALTI,   GUIO,
         KC_Z,   KC_X,   KC_C,   KC_D,   KC_V,   KC_NO,  KC_NO,  KC_K,   KC_H,   KC_COMM,KC_DOT, KC_SLSH,
         TO_BASE,KC_NO,  KC_NO,  KC_LSFT,BSPMSYM,ESCNUM, KC_ENT, SPCMNAV,KC_TAB, KC_NO,  KC_NO,  KC_NO),
 
@@ -79,7 +79,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_NAVIGATION_MAC] = LAYOUT_ortho_5x12(
         KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
         KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_PGUP,KC_UP,  KC_PGDN,KC_NO,
-        KC_LCTL,KC_LALT,KC_LSFT,KC_LGUI,KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_LEFT,KC_DOWN,KC_RGHT,KC_NO,
+        KC_LGUI,KC_LALT,KC_LSFT,KC_LCTL,KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_LEFT,KC_DOWN,KC_RGHT,KC_NO,
         KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
         TO_BASE,KC_NO,  KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_NO,  TO_MAC)
 };
